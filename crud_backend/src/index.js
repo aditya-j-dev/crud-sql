00f5728dfd86890a9b7a2019f51383a75cd44ed3
aditya-j-dev <adityaj2206@gmail.com>
2025-08-10 16:28:51 +0530

    deployment

--- crud_backend/src/index.js
+++ crud_backend/src/index.js
@@ -5,7 +5,9 @@ import clientRoutes from "./routes/clientRoute.js"
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 
 app.use('/api', clientRoutes);
